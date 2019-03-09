56a853982c8074b7069bed975b60fe7493fd5fe4
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2019-03-09 12:01:10 +0100

    add App entry to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { ThemeProvider } from "./utils/styled-components";
-import { theme } from "./utils/theme";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { ThemeProvider } from 'src/utils/styled-components';
+import { theme } from 'src/utils/theme';
+import App from 'src/containers/App';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <div>test</div>
+    <App />
   </ThemeProvider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
