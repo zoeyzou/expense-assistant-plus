9fff1f98a43abd084005ee3c2ed269ba7599821d
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2019-03-09 16:20:08 +0100

    add navigations component

---
+++ src/components/Navigations.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import NavigationLink from './NavigationLink';
+import HomeIcon from './Icons/HomeIcon';
+import ExpenseIcon from './Icons/ExpenseIcon';
+import AddIcon from './Icons/AddIcon';
+
+interface NavigationsProps {}
+
+const Navigations: React.FunctionComponent<NavigationsProps> = props => {
+  return (
+    <nav>
+      <NavigationLink
+        toPath='/'
+        icon={(className: string) => <HomeIcon className={className} />}
+        title='home'
+      />
+      <NavigationLink
+        toPath='/expenses'
+        icon={(className: string) => <ExpenseIcon className={className} />}
+        title='all expense'
+      />
+      <NavigationLink
+        toPath='/expenses/add'
+        icon={(className: string) => <AddIcon className={className} />}
+        title='add expense'
+      />
+    </nav>
+  );
+};
+
+export default Navigations;
