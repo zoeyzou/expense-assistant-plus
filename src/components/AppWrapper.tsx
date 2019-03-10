8a0342d60d545fea5b4f5b4d9d2255d103781f2f
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2019-03-10 18:04:29 +0100

    change app wrapper to center children

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -4,6 +4,8 @@ import { StyledComponentsProps } from 'src/models/styled-components-props';
 const AppWrapper = styled.div<StyledComponentsProps>`
   display: flex;
   flex-direction: row;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   height: 100%;
 `;
