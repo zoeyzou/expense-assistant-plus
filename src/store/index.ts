6973d19c8e545a5fcf569408d337278c6c88d867
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2019-03-10 18:05:43 +0100

    add api reducer in reducers

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers, applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { userReducer } from './user/reducers';
+import { apiReducer } from './api/reducers';
 
 const rootReducer = combineReducers({
   userState: userReducer,
+  apiState: apiReducer,
 });
 
 export type AppState = ReturnType<typeof rootReducer>;
