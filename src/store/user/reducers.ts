93d21b9f387e5ea6f3e12ac0b186f6bb6750af7a
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2019-03-10 22:19:19 +0100

    use ts intellisense to do switch case

--- src/store/user/reducers.ts
+++ src/store/user/reducers.ts
@@ -1,10 +1,4 @@
-import {
-  UserState,
-  UserActionType,
-  GET_USER_REQUEST,
-  GET_USER_SUCCESS,
-  GET_USER_FAILURE,
-} from './types';
+import { UserState, UserActionType } from './types';
 
 const initialState: UserState = {
   user: { name: '' },
@@ -15,17 +9,17 @@ export const userReducer = (
   action: UserActionType
 ): UserState => {
   switch (action.type) {
-    case GET_USER_REQUEST:
+    case 'GET_USER_REQUEST':
       return {
         ...state,
         ...{ loadingState: 'pending' },
       };
-    case GET_USER_SUCCESS:
+    case 'GET_USER_SUCCESS':
       return {
         ...state,
         ...{ loadingState: 'success', user: action.payload.user },
       };
-    case GET_USER_FAILURE:
+    case 'GET_USER_FAILURE':
       return {
         ...state,
         ...{ loadingState: 'failure', error: action.payload.error },
