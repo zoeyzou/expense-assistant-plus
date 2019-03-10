087175fce5d93d114118ff97574aa0fd138db741
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2019-03-10 20:55:28 +0100

    add min width to menu wrapper

--- src/components/MenuWrapper.tsx
+++ src/components/MenuWrapper.tsx
@@ -4,6 +4,7 @@ export const MenuWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: calc(250px + 5vw);
+  min-width: 270px;
   height: 100%;
   padding: calc(30px + 2vh) 0 calc(10px + 2vh);
   background-color: ${({ theme }) => theme.color.darkOrange};
