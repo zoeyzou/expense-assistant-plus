4847753c5fb41e5dbc22e5169fb0eb150789bec6
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2019-03-14 22:47:44 +0100

    add table wrapper

---
+++ src/components/TableWrapper.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import styled from 'src/utils/styled-components';
+import { StyledComponentsProps } from 'src/models/styled-components-props';
+import Flex, { FlexProps } from 'src/components/Flex';
+
+type TableWrapperProps = StyledComponentsProps &
+  FlexProps & {
+    className?: string;
+  };
+
+const _TableWrapper: React.FunctionComponent<TableWrapperProps> = ({
+  className,
+  children,
+  ...props
+}) => {
+  return (
+    <Flex {...props} padding='10px 0' margin='10px auto' className={className}>
+      {children}
+    </Flex>
+  );
+};
+
+const TableWrapper = styled(_TableWrapper)`
+  border-top: 1px solid grey;
+  border-bottom: 1px solid grey;
+
+  & > :nth-of-type(odd) {
+    background-color: ${({ theme }) => theme.color.lightWhite};
+  }
+`;
+
+export default TableWrapper;
